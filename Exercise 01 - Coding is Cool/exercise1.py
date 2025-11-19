54043857a1326c5b63dc4f3ee3211a3d8bb63594
prithvinisheed1234-sys <prithvinisheed1234@gmail.com>
2025-11-19 20:08:13 +0400

    exercise 1

---
+++ Exercise 01 - Coding is Cool/exercise1.py
@@ -0,0 +1,8 @@
+# declaring the variable word1
+word1 = "Coding"
+# declaring the variable word2
+word2 = " is"
+# declaring the variable word3
+word3 = " Cool"
+# diaplaying the sentence on screen
+print(word1+" "+word2+" "+word3)
