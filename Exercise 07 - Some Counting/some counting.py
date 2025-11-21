448133f38ed1fc1f21143cedc469e8582ef2e82b
prithvinisheed1234-sys <prithvinisheed1234@gmail.com>
2025-11-21 18:38:50 +0400

    some counting

---
+++ Exercise 07 - Some Counting/some counting.py
@@ -0,0 +1,20 @@
+# Some Counting Exercises
+print("Counting up to 0 to 50")
+for i in range(0,51):
+    print(i)
+# Write a for loop that counts down from 50 to 0 (inclusive) and prints each number.
+print("Counting down from 50 to 0")
+for i in range(50,-1,-1):
+    print(i)
+# Write a for loop that counts up from 30 to 50 (inclusive) and prints each number.
+print("Counting up from 30 to 50")
+for i in range(30,51):
+    print(i)
+# Write a for loop that counts down from 50 to 10 (inclusive) by 2 and prints each number.
+print("Counting down from 50 to 10 by 2")
+for i in range(50,9,-2):
+    print(i)
+# Write a for loop that counts up from 100 to 200 (inclusive) by 5 and prints each number.
+print("Counting up from 100 to 200 by 5")
+for i in range(100,201,5):
+    print(i)
