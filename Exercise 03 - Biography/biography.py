08f3e44fa6f9328dd342ab3a74f4509b0376bc31
prithvinisheed1234-sys <prithvinisheed1234@gmail.com>
2025-11-21 17:56:58 +0400

    biography

---
+++ Exercise 03 - Biography/biography.py
@@ -0,0 +1,8 @@
+# declaring the variable name.
+Name = "Prithvi"
+# declaring the variable hometown.
+Hometown = "Kerala"
+# declaring the variable age.
+Age = 19
+# printing the biography using the variables declared above.
+print("my name is " + Name+"\n my hometown is " + Hometown + "\n and I am " + str(Age) + " years old.")
