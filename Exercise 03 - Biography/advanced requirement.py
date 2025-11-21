e98b6f737d39673787e5898a195a6ba53a892405
prithvinisheed1234-sys <prithvinisheed1234@gmail.com>
2025-11-21 18:17:45 +0400

    advanced requirements

--- Exercise 03 - Biography/advanced requirement.py
+++ Exercise 03 - Biography/advanced requirement.py
@@ -1,5 +1,10 @@
+# decaring the variable first_name.
 first_name=input("Enter your first name: ")
+# declaring the variable second_name.
 second_name=input("Enter your second name: ")
+# declaring the variable age.
 age=(input("Enter your age: "))
+# declaring the variable hometown.
 hometown=input("Enter your hometown: ")
+# printing the biography using the variables declared above.
 print("My name is " + first_name + " " + second_name + "\nMy hometown is " + hometown + "\nAnd I am " + str(age) + " years old.")
