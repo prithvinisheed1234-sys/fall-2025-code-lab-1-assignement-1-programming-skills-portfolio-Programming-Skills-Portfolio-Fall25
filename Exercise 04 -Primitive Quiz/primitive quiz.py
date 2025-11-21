151c92004ba26500e8098e797bc711b2e50689ae
prithvinisheed1234-sys <prithvinisheed1234@gmail.com>
2025-11-21 18:17:59 +0400

    primitive quiz

---
+++ Exercise 04 -Primitive Quiz/primitive quiz.py
@@ -0,0 +1,10 @@
+# asking the user the question and storing the input in the variable answer.
+answer = input("What is the capital of France?")
+# checking if the answer is correct or not and printing the appropriate message.
+if answer == "Paris":
+    # printing the message if the answer is correct.
+    print("The answer is correct")
+# printing the message if the answer is incorrect.
+else:
+    # printing the message if the answer is incorrect.
+    print("The answer is incorrect")
