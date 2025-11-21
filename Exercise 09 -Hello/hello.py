719f76b092b703325024a901c2e6333929c5fd75
prithvinisheed1234-sys <prithvinisheed1234@gmail.com>
2025-11-21 18:43:42 +0400

    hello

---
+++ Exercise 09 -Hello/hello.py
@@ -0,0 +1,9 @@
+# function to print "Hello"
+def hello():
+    print("Hello")  
+# main function to call hello
+def main():
+    hello()  
+# entry point of the script
+if __name__ == "__main__":
+    main()
